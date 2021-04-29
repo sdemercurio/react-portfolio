c0f67e5458919d07d04e4e59869acc09987d48e9
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-29 11:17:26 -0400

    changing to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Web from './pages/Web';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
