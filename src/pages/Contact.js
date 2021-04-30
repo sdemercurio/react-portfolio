0780eb18bcd3cac1a010d88b54e3344bf6e26541
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-04-30 17:33:47 -0400

    fixing responsiveness of Contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from 'react-bootstrap';
 import Title from '../components/Title';
 import '../styles/contact.css';
 import '../index.css';
@@ -10,17 +11,27 @@ const Contact = () => {
             <div>
                 <Title title={"Let's Work Together"} />
             </div>
-            <div className="contact-body">
-                <div className="contact-info">
-                <p>
-                    If you have an idea and would like a creative mind to help you execute it, I would love to hear about it. Please feel free to reach out!
-                </p>
-                <a className="email-link" href="mailto:sarah.l.demercurio@gmail.com" rel="noopener noreferrer" target="_blank">Say Hello</a>
-                </div>
-            </div>
+            <Container>
+                <Row className="contact-body">
+                    <Col md={2}></Col>
+                    <Col md={8} className="contact-info">
+                            <p>
+                                If you have an idea and would like a creative mind to help you execute it, I would love to hear about it. Please feel free to reach out!
+                            </p>
+                        </Col>
+                        <Col md={2}></Col>
+                </Row>
+                <Row className="say-hi">
+                <Col md={5}></Col>
+                <Col md={2} className="email-link">
+                        <a className="btn" href="mailto:sarah.l.demercurio@gmail.com" rel="noopener noreferrer" target="_blank">Say Hello</a>
+                </Col>
+                <Col md={5}></Col>
+                </Row>
+            </Container>
+
             <Footer />
-            <div className="shape1"></div>
-            
+
 
         </>
     );
