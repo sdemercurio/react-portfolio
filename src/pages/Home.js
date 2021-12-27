e5dda54dbc5d081a4596f5c496d4d1d157a0e9d0
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-12-27 10:10:07 -0500

    fixed footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,21 +3,31 @@ import Title from '../components/Title';
 import Footer from '../components/Footer';
 const Home = () => {
     return (
-        <div className="home-content">
-            <Title title={'Sarah L. DeMercurio'} />
-            <div className="container">
-                <div className = "row">
-                    <div className = "col-md-6">
-                <img className='portrait' src={portrait} alt="Portrait of Sarah" />
+        <>
+            <div className="home-content">
+                <Title title={'Sarah L. DeMercurio'} />
+                <div className="home-container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <img className='portrait' src={portrait} alt="Portrait of Sarah" />
+                        </div>
+                        <div className="info col-md-6">
+                            <p>I am a graphic designer making a leap toward web developer.
+                                I earned a certification in full-stack web development from 
+                                Michigan State University in May of 2021, and am now looking 
+                                for an opportunity to grow as a front-end web developer. 
+                            </p>
+                            <p>Although I am most familiar with MERN stack, I am constantly learning 
+                                new frameworks to see what fits best and because, well, there's a lot 
+                                to learn out there!</p>
+                            <p>Come on in, take a look around, and maybe we can build something cool together.</p>
+                        </div>
+                    </div>
                 </div>
-                <div className = "info col-md-6">
-                <p>Sarah is a <span className="webdev">designer</span> and <span className="webdev"> web developer</span> who hails from the magical mitten.</p>
-                </div>
-                </div>
+                
             </div>
             <Footer />
-        </div>
-
+        </>
     );
 }
 
--- src/pages/Web.js
+++ src/pages/Web.js
@@ -4,19 +4,25 @@ import Projects from '../components/Project';
 import WebProjects from '../components/WebProjects';
 import '../styles/projects.css';
 import { useState } from 'react';
-
+import Footer from '../components/Footer';
 const Web = () => {
-    const [webProjects ] = useState(Projects);
+    const [webProjects] = useState(Projects);
 
     return (
-        <div className="PortfolioPage">
-            <div className="title">
-           <Title title={'Web Projects'}/>
-           </div>
-           <div className="projects">
-                <WebProjects webProject={webProjects}/>
-           </div>
-        </div>
+        <>
+            <div className="PortfolioPage">
+                <div className="title">
+                    <Title title={'Web Projects'} />
+                </div>
+                <div className="projects-container">
+                    <div className="projects">
+                        <WebProjects webProject={webProjects} />
+                    </div>
+                </div>
+            </div>
+<Footer />
+
+        </>
     );
 }
 
